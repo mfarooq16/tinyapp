9423f821d347544a078f6e5ce4bc17b49bac9ba9
vagrant <maham.16491@gmail.com>
2020-09-01 06:37:43 +0000

    tiny app

--- express_server.js
+++ express_server.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+
+// use res.render to load up an ejs view file
+
+//route handler for urls
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
