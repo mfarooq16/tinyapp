da4140522f3e95b59a459f3fa60747922e4b299f
vagrant <maham.16491@gmail.com>
2020-09-01 20:20:13 +0000

    tiny app

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 // set the view engine to ejs
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 const urlDatabase = {
@@ -10,6 +12,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function()  {
+  let r = Math.random().toString(36).substring(7);
+  //console.log("random", r);
+  return r;
+}
 
 // use res.render to load up an ejs view file
 
@@ -19,11 +26,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
