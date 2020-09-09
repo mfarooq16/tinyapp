3500a67c0887e83273937ec8d0769e857f87bcbc
vagrant <maham.16491@gmail.com>
2020-09-09 05:56:43 +0000

    setting up for submission

--- helpers.js
+++ helpers.js
@@ -1,5 +1,23 @@
 const bcrypt = require('bcrypt');
 
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 const generateRandomString = function()  {
   let r = Math.random().toString(36).substring(7);
   return r;
